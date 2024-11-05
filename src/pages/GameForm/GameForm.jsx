9433379f86e36c15da188b5aed539f98ace03de3
Justin Fanton <justin.fanton@gmail.com>
2024-11-05 11:16:03 +0100

    Implemented review form functionality

---
+++ src/pages/GameForm/GameForm.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+function GameForm({ onSubmitReview }) {
+    const [username, setUsername] = useState("");
+    const [comment, setComment] = useState("");
+    const [rating, setRating] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(username && comment && rating >= 0 && rating <= 10) {
+            onSubmitReview({ username, comment, rating });
+            setUsername("");
+            setComment("");
+            setRating("");
+        } else {
+            alert("Please fill out all fields and provide a rating between 0 and 10.")
+        }
+    };
+
+    return (
+        <form className="game-form" onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                />
+            </label>
+            <label>
+                Comment:
+                <input
+                type="text"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                required
+                />
+            </label>
+            <label>
+                Rating:
+                <input
+                type="number"
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
+                min="0"
+                max="10"
+                required
+                />
+            </label>
+            <button type="submit">Submit Review</button>
+        </form>
+    );
+}
+
+export default GameForm;
