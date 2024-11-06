8a2b81044381710a4b11b5077e975112ca5fdb55
Justin Fanton <justin.fanton@gmail.com>
2024-11-06 12:57:37 +0100

    New css backgrounds for HomePage + About us page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../HomePage/homePage.css";
 
 function HomePage() {
+  useEffect(() => {
+    // Add a class to body when on HomePage
+    document.body.classList.add('home-page-background');
+    document.body.style.overflowY = 'hidden';
+
+    return () => {
+      // Remove class and add y-axis scroller to body when leaving HomePage
+      document.body.classList.remove('home-page-background');
+      document.body.style.overflowY = ''; 
+    };
+  }, []);
+
   return (
     <div className="home-page">
       <header className="home-header">
