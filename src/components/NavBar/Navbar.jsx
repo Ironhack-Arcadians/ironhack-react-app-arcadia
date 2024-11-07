415271ae99ded39ad9633946070e7bf9bda4d345
Justin Fanton <justin.fanton@gmail.com>
2024-11-07 11:24:35 +0100

    Implemented search bar and rendered it only on the gameList page

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,8 +1,27 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 import logo from "../../images/Logo.png";
 import "./NavBar.css";
 
-function Navbar() {
+function Navbar({ handleSearch }) {
+  const [query, setQuery] = useState("");
+  const location = useLocation();
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSearchClick = () => {
+    handleSearch(query);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch(query);
+    }
+  };
+
+
   return (
     <nav className="navbar">
       <div className="logo-container">
@@ -10,11 +29,27 @@ function Navbar() {
           <img src={logo} alt="Logo" className="logo" />
         </NavLink>
       </div>
-      
+
       <div className="title-container">
         <h1>[Arcadia]</h1>
       </div>
+
       <div className="home-buttons">
+        {location.pathname === "/catalogue" && (
+          <div className="search-container">
+            <input
+              type="text"
+              placeholder="Search games"
+              value={query}
+              onChange={handleInputChange}
+              onKeyPress={handleKeyPress}
+              className="search-bar"
+            />
+            <button onClick={handleSearchClick} className="search-button">
+              Search
+            </button>
+          </div>
+        )}
         <NavLink to="/catalogue" className="home-button">
           Browse Games
         </NavLink>
